8a37e8873d78c3102cb6d588adde1eb7962ffa82
NayeraAyman20 <120630150+NayeraAyman20@users.noreply.github.com>
2022-12-23 21:24:31 +0200

    Update AnimListener.java

--- AnimListener.java
+++ AnimListener.java
@@ -1,11 +1,8 @@
 
-
 package Textures;
 
 import java.awt.event.KeyListener;
 import javax.media.opengl.GLEventListener;
-
-
 
 public abstract class AnimListener implements GLEventListener, KeyListener {
  
